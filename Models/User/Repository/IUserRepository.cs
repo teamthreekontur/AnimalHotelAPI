25936fb7bdeb467d2fcd2ce43904dd2b95973531
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2019-04-06 15:39:04 +0700

    Create project "Models" and create model "User".

---
+++ Models/User/Repository/IUserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Models.User.Repository
+{
+    /// <summary>
+    /// Интерфейс, описывающий хранилище пользователей
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Создать нового пользователя
+        /// </summary>
+        /// <param name="createInfo">Данные для создания нового пользователя</param>
+        /// <returns>Созданный пользователь</returns>
+        User Create(UserCreateInfo createInfo);
+
+        /// <summary>
+        /// Получить пользователя по идентификатору
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <returns>Пользователь</returns>
+        User Get(Guid userId);
+
+
+        User Patch(UserPatchInfo patchInfo);
+    }
+}
---
+++ Models/User/UserCreateInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.User
+{
+    /// <summary>
+    /// Информация для создания пользователя
+    /// </summary>
+    public class UserCreateInfo
+    {
+        /// <summary>
+        /// Инийиализирует новый экземпляр описания для создания пользователя
+        /// </summary>
+        /// <param name="login">Логин пользователя</param>
+        /// <param name="password">Хэш пароля</param>
+        public UserCreateInfo(string login, string password)
+        {
+            if (login == null)
+            {
+                throw new ArgumentNullException(nameof(login));
+            }
+
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            this.Login = login;
+            this.Passwod = password;
+        }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; }
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Passwod { get; }
+    }
+}
