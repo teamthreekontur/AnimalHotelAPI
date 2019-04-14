4c83350d4a7c3e259def18c86735907a528c4add
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2019-04-14 15:07:49 +0700

    Edited modul "Place".

---
+++ Models/Place/PlaceCreateInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Models.Place
+{
+    /// <summary>
+    /// Информация для создания передержки
+    /// </summary>
+    class PlaceCreateInfo
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр описания для создания передержки
+        /// </summary>
+        /// <param name="name">Название передержки</param>
+        /// <param name="address">Адрес передержки</param>
+        public PlaceCreateInfo(string name, string address)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        /// <summary>
+        /// Название передержки
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Адрес передержки
+        /// </summary>
+        public string Address { get; }
+
+        /// <summary>
+        /// Id владельца передержки
+        /// </summary>
+        public Guid IdOwner { get; }
+    }
+}
---
+++ Models/Place/Repository/MemoryPlaceRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.Place.Repository
+{
+    class MemoryPlaceRepository : IPlaceRepository
+    {
+        private readonly Dictionary<Guid, Place> primaryKeyIndex;
+
+        /// <summary>
+        /// Создает новый экземпляр хранилища передержек в памяти
+        /// </summary>
+        public MemoryPlaceRepository()
+        {
+            primaryKeyIndex = new Dictionary<Guid, Place>();
+        }
+
+        /// <summary>
+        /// Создать новую передержку
+        /// </summary>
+        /// <param name="creationInfo">Информация для создания передержки</param>
+        /// <returns>Информация о созданной передержке</returns>
+        public Place Create(PlaceCreateInfo createInfo)
+        {
+            if (createInfo == null)
+            {
+                throw new ArgumentNullException(nameof(createInfo));
+            }
+
+            var id = Guid.NewGuid();
+
+            var place = new Place
+            {
+                Id = id,
+                Name = createInfo.Name,
+                Address = createInfo.Address,
+                IdOwner = createInfo.IdOwner
+            };
+
+            primaryKeyIndex.Add(id, place);
+
+            return place;
+        }
+
+        /// <summary>
+        /// Удалить передержку по идентификатору
+        /// </summary>
+        /// <param name="placeId">Идентификатор передержки</param>
+        /// <returns>Передержка</returns>
+        public Place Delete(Guid placeId)
+        {
+            var place = Get(placeId);
+            primaryKeyIndex.Remove(placeId);
+
+            return place;
+        }
+
+        /// <summary>
+        /// Получить передержку по идентификатору
+        /// </summary>
+        /// <param name="placeId">Идентификатор передержки</param>
+        /// <returns>Передержка</returns>
+        public Place Get(Guid placeId)
+        {
+            if (!primaryKeyIndex.TryGetValue(placeId, out var place))
+            {
+                throw new PlaceNotFoundException(placeId);
+            }
+
+            return place;
+        }
+
+        public Place[] Get(PlaceFilterInfo placeFilter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Place Patch(PlaceCreateInfo patchInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
