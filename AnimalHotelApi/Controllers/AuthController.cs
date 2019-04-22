632c506a405edbc690702da9fc01b17df3636570
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2019-04-22 19:44:40 +0700

    Create AuthController.

---
+++ AnimalHotelApi/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Models.User;
+using Models.User.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AnimalHotelApi.Controllers
+{
+    public class AuthController : ApiController
+    {
+        private readonly IUserRepository repository;
+        private readonly IAuthenticator authenticator;
+
+        public AuthController(IUserRepository repository, IAuthenticator authenticator)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            this.repository = repository;
+            this.authenticator = authenticator;
+        }
+
+        public IActionResult Registration([FromBody] UserRegistrationInfo userRegistrationInfo)
+        {
+            var userInfo = this.repository.Create(new UserCreationInfo(userRegistrationInfo.Login, userRegistrationInfo.Password));
+            return this.Ok();
+        }
+
+        public IActionResult Enter([FromBody] UserRegistrationInfo userRegistrationInfo)
+        {
+            var user = this.authenticator.AuthenticateAsync(userRegistrationInfo.Login, userRegistrationInfo.Password);
+            return this.Ok();
+        }
+    }
+}
