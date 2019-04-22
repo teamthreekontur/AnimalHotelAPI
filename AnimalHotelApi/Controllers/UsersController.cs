061924accbade7aeb660ae268849072940722826
Ignis Deus <ignisdeus2048@gmail.com>
2019-04-22 19:23:10 +0700

    Add swagger

---
+++ AnimalHotelApi/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Models.User;
+using Models.User.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AnimalHotelApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public IHttpActionResult Patch(string guid, [FromBody]string value)
+        {
+            return this.BadRequest();
+        }
+    }
+}
